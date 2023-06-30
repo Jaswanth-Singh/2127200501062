9d55076b59cd4e8a2caee7d0f42f4401b2ed2d3c
Jaswanth Singh <jasujaswanthsingh@gmail.com>
2023-06-30 20:53:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{alignContent:'center'}}>
+        <div>Login</div>
+        <input type="text" placeholder="Name" class="form-control-lg"/>
+        <input type="text" placeholder="Password" class="form-control-lg"/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Trains.js
@@ -0,0 +1,6 @@
+function Trains(){
+    
+    return 
+}
+
+export default Trains;
